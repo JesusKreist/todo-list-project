fa94b15af7d754a6bd185ddf7b2c1bbf8633066f
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2020-07-17 20:30:24 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -34,10 +34,29 @@ localStorage.getObj("state") ? state = localStorage.getObj("state") : state = {}
 
 
 // add the html stored in the values of the todoList object to the document body.
-
 for (let i of Object.values(todoList)) {
     items.insertAdjacentHTML("beforebegin", i);
 };
+
+console.log(Object.keys(state).length)
+
+if (Object.keys(state).length > 0) {
+    for (let i of Object.keys(state)) {
+        let workingDocument = document.getElementById(i).firstElementChild;
+        let taskState = state[i];
+        console.log(taskState);
+        if (taskState === 1) {
+            console.log("It's pending now!");
+            workingDocument.classList.add("completed");
+            console.log("It's done now!");
+        } else {
+            console.log("It's done now!");
+            workingDocument.classList.remove("completed");
+            console.log("Resetting to pending");
+        };
+    };
+};
+
 
 
 // clear the local storage, loop through all the keys in the todolist == the div ids
@@ -50,6 +69,7 @@ const clearAll = () => {
         document.getElementById(i).remove();
     };
     todoList = {};
+    state = {};
 };
 
 
@@ -57,35 +77,49 @@ const clearAll = () => {
 const deleteEntry = event => {
     let entry = event.target.parentNode.parentNode.parentNode;
     entry.remove();
+    console.log(entry.id);
+    console.log(state);
     delete todoList[entry.id];
-    localStorage.setObj("todoItems", todoList)
+    delete state[entry.id];
+    localStorage.setObj("todoItems", todoList);
+    localStorage.setObj("state", state);
 };
-
 
 
 const markComplete = event => {
+    let entry = event.target.parentNode.parentNode.parentNode;
+    let taskState = state[entry.id];
+    console.log(taskState);
     let text = event.target.parentNode.parentNode.parentNode.firstElementChild;
     
-    if (complete === 0) {
-        console.log("It's off")
+    if (taskState === 0) {
+        console.log("It's pending now!");
         text.classList.add("completed");
-        complete++
+        console.log("It's done now!");
+        taskState++;
     } else {
-        console.log("It's on")
+        console.log("It's done now!");
         text.classList.remove("completed");
-        complete--
-    }
+        console.log("Resetting to pending");
+        taskState--;
+    };
+    state[entry.id] = taskState;
+    localStorage.setObj("state", state);
 };
+
 
 const editEntry = event => {
     let entry = event.target.parentNode.parentNode.parentNode.firstElementChild;
     inputField.value = entry.innerHTML;
     entry = event.target.parentNode.parentNode.parentNode;
     entry.remove();
+    console.log(state);
     delete todoList[entry.id];
+    delete state[entry.id];
     localStorage.setObj("todoItems", todoList);
-
+    localStorage.setObj("state", state);
 };
+
 
 for (let e of Object.keys(todoList)) {
     const deleteBtn = document.getElementById(e).querySelector(".delete-item");
@@ -95,6 +129,7 @@ for (let e of Object.keys(todoList)) {
     const editBtn = document.getElementById(e).querySelector(".edit-item");
     editBtn.addEventListener("click", editEntry);
 };
+
 
 const addItem = () => {
     let newEntry = inputField.value;
@@ -113,8 +148,10 @@ const addItem = () => {
         counter++
         // add the item to the local storage
         todoList[itemId] = newItem;
+        state[itemId]  = 0;
         inputField.value = "";
         localStorage.setObj("todoItems", todoList);
+        localStorage.setObj("state", state);
         // check if the item is in the document, if it's not... delete it.
         // let checkDocument = document.getElementById(itemId);
         items.insertAdjacentHTML("beforebegin", newItem);
@@ -127,10 +164,6 @@ const addItem = () => {
 
     }
 };
-
-
-
-
 
 
 const testSubmit = event => {
